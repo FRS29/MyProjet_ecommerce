ee822b1e109c38c8fe1b31af431b217601bd3854
FRS29 <156894523+FRS29@users.noreply.github.com>
2024-11-11 19:36:03 +0100

    Add files via upload

---
+++ classes/CRUD_produit.php
@@ -0,0 +1,12 @@
+<?php
+require_once "produit.php";
+class CRUD_produit
+{
+    private $pdo;
+    public function __construct() {}
+    public function add() {}
+    public function delete() {}
+    public function find() {}
+    public function finAll() {}
+    public function edit() {}
+}
